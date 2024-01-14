898c9b575d1b2ff29546fa2b8cd16f011559d769
D4rkGh057 <erickfuelpaz@gmail.com>
2024-01-13 20:17:57 -0500

    se elimina los archivos innecesarios

--- conexion.php
+++ conexion.php
@@ -7,7 +7,10 @@ class Conexion
         define("usuario", "root");
         define("contrasena", "5YUgOARflPnvYROYT9YnidwsSuDiNqRp");
         define("database", "pruebas");
-        $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND > 'SET NAMES utf8');
+        $opc = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND > 'SET NAMES utf8',
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+        );
         try {
             $conexion = new PDO("mysql:host=" . host . ";dbname=" . database, usuario, contrasena, $opc);
             return $conexion;
